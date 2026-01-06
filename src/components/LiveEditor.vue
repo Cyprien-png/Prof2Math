a1f14a045861dd94c90efbc38d57110df1b6d057
Cyprien-png <cyprien04@hotmail.com>
2026-01-06 11:29:08 +0100

    fix: trim block whitespace on save

--- src/components/LiveEditor.vue
+++ src/components/LiveEditor.vue
@@ -81,6 +81,9 @@ const saveBlock = (index: number) => {
         return;
     }
 
+    // Trim content
+    block.markdown = block.markdown.trim();
+
     block.html = DOMPurify.sanitize(md.render(block.markdown));
     block.isEditing = false;
 };
